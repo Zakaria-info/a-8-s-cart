13639310fce2b66d3be50ccc67fff92d07b3500b
Zakaria-info <info.zakariahossain01@gnail.com>
2026-05-04 21:13:08 +0600

    Solving deployment issue

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,4 @@
 
-
-
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
@@ -10,16 +8,18 @@ const db = client.db("a-8-summer-cart");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
     client,
   }),
   emailAndPassword: { 
     enabled: true, 
   }, 
-   socialProviders: {
-        google: { 
-            clientId: process.env.GOOGLE_CLIENT_ID, 
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
-        }, 
-    },
+  socialProviders: {
+    google: { 
+      clientId: process.env.GOOGLE_CLIENT_ID, 
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
+    }, 
+  },
+  trustedOrigins: [
+    process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
+  ],
 });
